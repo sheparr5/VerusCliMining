79fa388da9ccc39e6e976a609318f17e4b8f7a30
sheparr5 <106758422+sheparr5@users.noreply.github.com>
2024-03-31 10:37:35 -0400

    Update rewrite.sh

--- rewrite.sh
+++ rewrite.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i 's/us.vipor.net:5045/na.luckpool.net:3960/g' ccminer/config.json &&
+sed -i 's/us.vipor.net:5045/na.luckpool.net:3960/g' ccminer/config.json
 sed -i 's/us.vipor.net:5040/na.luckpool.net:3960/g' ccminer/config.json
